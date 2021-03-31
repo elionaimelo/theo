bf652d6cbe7b00caf90fa5ecca526e08c5f5c4ac
Elionai Melo <elionai.melo@lais.huol.ufrn.br>
2021-03-31 11:52:46 -0300

    build: inital screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:theo/core/routes.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 import 'package:theo/pages/login_screen/login_screen.dart';
 import 'package:theo/pages/splash_screen/splash_screen.dart';
+
+import 'package:theo/core/routes.dart';
+
+import 'package:theo/constant.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,10 +17,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Theo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: kprimaryColor,
+        textTheme: GoogleFonts.muliTextTheme(
+          Theme.of(context).textTheme,
+        ),
       ),
       routes: {
         Routes.splash: (context) => SplashScreen(),
--- lib/pages/login_screen/login_screen.dart
+++ lib/pages/login_screen/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:theo/constant.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -8,8 +9,99 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+              flex: 1,
+              child: Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/bg1.png'),
+                        fit: BoxFit.cover)),
+              )),
+          Expanded(
+              child: Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 30),
+                child: FittedBox(
+                  fit: BoxFit.contain,
+                  child: Image.asset('assets/images/logo-theo.png'),
+                ),
+              ),
+              Container(
+                width: 262,
+                height: 80,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 24.0),
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                        text:
+                            'Veja histórias em qualquer\n lugar e compartilhe com facilidade!',
+                        style: Theme.of(context).textTheme.bodyText1.copyWith(
+                            color: kprimaryColor, fontWeight: FontWeight.w600)),
+                  ),
+                ),
+              ),
+              Container(
+                  margin: const EdgeInsets.only(top: 30, right: 10, left: 10),
+                  child: Column(
+                    children: [
+                      TextButton(
+                        style: TextButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0)),
+                            primary: Colors.white,
+                            backgroundColor: kprimaryColor,
+                            textStyle: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w900)),
+                        onPressed: () {},
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Criar perfil',
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 20),
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0)),
+                              primary: kprimaryColor,
+                              backgroundColor: klightColor,
+                              textStyle: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.w900)),
+                          onPressed: () {},
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'Já tenho uma conta',
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ))
+            ],
+          ))
+        ],
+      ),
     );
   }
 }
